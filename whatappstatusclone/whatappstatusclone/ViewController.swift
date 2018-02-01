50b278e7800f7be794c45b94d3292a72a8ff0232
Tibin Thomas <tibinthomas@Tibin-macbook.local>
2018-02-01 12:45:33 +0530

    project init

---
+++ whatappstatusclone/whatappstatusclone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whatappstatusclone
+//
+//  Created by Tibin Thomas on 01/02/18.
+//  Copyright © 2018 tibinT9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
